216b1701fa59f6d79641142539ac42a8fc74570f
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-09-16 21:32:00 +0200

    Add code to fetch request

--- ping_server.py
+++ ping_server.py
@@ -8,3 +8,14 @@ url = os.getenv("SERVER_URL")
 
 if not url:
     print("SERVER_URL is not set in the environment variables.")
+
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f"Ping successful: {response.status_code}")
+    else:
+        print(f"Ping failed: {response.status_code}")
+
+except requests.exceptions.RequestException as e:
+
+    print(f"An error occurred: {e}")
